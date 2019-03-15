6799f5815c425d0fa796edd23476d2e403bb6146
mhs <mhs@cbrain.net>
2019-03-15 15:34:08 +0100

    added regiment class

---
+++ RegimentTest/RegimentTest/Classes/Regiment.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RegimentTest.Classes
+{
+  public class Regiment
+  {
+    public List<Character> Characters { get; set; }
+
+    public Regiment() { }
+
+    public void AttackRegiment(IAction action, CharacterType type, Regiment regiment)
+    {
+
+    }
+  }
+}
